f4557e43e0608c9ad8f573c2aedbe06cef228d51
David Sevilla <dsevilla192@icloud.com>
2018-07-20 10:30:59 -0400

    fix weird indentation thing

--- js/src/forum/components/UserUnsuspendedNotification.js
+++ js/src/forum/components/UserUnsuspendedNotification.js
@@ -14,7 +14,7 @@ export default class UserUnsuspendedNotification extends Notification {
   content() {
     const notification = this.props.notification;
 
-  return app.translator.trans('flarum-suspend.forum.notifications.user_unsuspended_text', {
+    return app.translator.trans('flarum-suspend.forum.notifications.user_unsuspended_text', {
       user: notification.sender(),
     });
   }
