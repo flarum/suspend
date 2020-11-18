f93270dfb7846e3b920c4b4bd6ef8830d7bec187
luceos <luceos@users.noreply.github.com>
2020-11-18 07:38:11 +0000

    Apply fixes from StyleCI

--- src/Listener/PreventAttributesMutations.php
+++ src/Listener/PreventAttributesMutations.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Flarum\Suspend\Listener;
 
 use Flarum\User\Event\Saving;
@@ -9,7 +16,9 @@ class PreventAttributesMutations
 {
     public function handle(Saving $event)
     {
-        if (! $event->user->suspended_until) return;
+        if (! $event->user->suspended_until) {
+            return;
+        }
 
         if ($event->user->id === $event->actor->id) {
             throw new PermissionDeniedException;
