49b9cd4ae2fab6bdc9c0b1068f1677180b015eec
SychO9 <sychocouldy@gmail.com>
2020-11-18 15:50:52 +0100

    Use new ApiSerializer extender

---
+++ src/AddUserSuspendAttributes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Suspend;
+
+use Flarum\Api\Serializer\UserSerializer;
+use Flarum\User\User;
+
+class AddUserSuspendAttributes
+{
+    public function __invoke(array $attributes, User $user, UserSerializer $serializer)
+    {
+        $canSuspend = $serializer->getActor()->can('suspend', $user);
+
+        if ($canSuspend) {
+            $attributes['suspendedUntil'] = $serializer->formatDate($user->suspended_until);
+        }
+
+        $attributes['canSuspend'] = $canSuspend;
+
+        return $attributes;
+    }
+}
