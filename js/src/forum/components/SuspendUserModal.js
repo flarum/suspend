e38751bcc5d889c6f4c82b01e9a15f241e537613
Ian Morland <ian@morland.me>
2021-02-18 18:13:16 +0000

    Move form items to itemlist for extensibility

--- js/src/forum/components/SuspendUserModal.js
+++ js/src/forum/components/SuspendUserModal.js
@@ -3,6 +3,7 @@ import Button from 'flarum/components/Button';
 
 import Stream from 'flarum/utils/Stream';
 import withAttr from 'flarum/utils/withAttr';
+import ItemList from 'flarum/utils/ItemList';
 
 export default class SuspendUserModal extends Modal {
   oninit(vnode) {
@@ -31,7 +32,7 @@ export default class SuspendUserModal extends Modal {
   }
 
   title() {
-    return app.translator.trans('flarum-suspend.forum.suspend_user.title', {user: this.attrs.user});
+    return app.translator.trans('flarum-suspend.forum.suspend_user.title', { user: this.attrs.user });
   }
 
   content() {
@@ -41,52 +42,9 @@ export default class SuspendUserModal extends Modal {
           <div className="Form-group">
             <label>{app.translator.trans('flarum-suspend.forum.suspend_user.status_heading')}</label>
             <div>
-              <label className="checkbox">
-                <input type="radio" name="status" checked={!this.status()} value="" onclick={withAttr('value', this.status)}/>
-                {app.translator.trans('flarum-suspend.forum.suspend_user.not_suspended_label')}
-              </label>
-
-              <label className="checkbox">
-                <input type="radio" name="status" checked={this.status() === 'indefinitely'} value='indefinitely' onclick={withAttr('value', this.status)}/>
-                {app.translator.trans('flarum-suspend.forum.suspend_user.indefinitely_label')}
-              </label>
-
-              <label className="checkbox SuspendUserModal-days">
-                <input type="radio" name="status" checked={this.status() === 'limited'} value='limited' onclick={e => {
-                  this.status(e.target.value);
-                  m.redraw.sync();
-                  this.$('.SuspendUserModal-days-input input').select();
-                  e.redraw = false;
-                }}/>
-                {app.translator.trans('flarum-suspend.forum.suspend_user.limited_time_label')}
-                {this.status() === 'limited' ? (
-                  <div className="SuspendUserModal-days-input">
-                    <input type="number"
-                      min="0"
-                      value={this.daysRemaining()}
-                      oninput={withAttr('value', this.daysRemaining)}
-                      className="FormControl"/>
-                    {app.translator.trans('flarum-suspend.forum.suspend_user.limited_time_days_text')}
-                  </div>
-                ) : ''}
-              </label>
+              {this.formItems().toArray()}
             </div>
           </div>
-
-          <div className="Form-group">
-            <label>
-              {app.translator.trans('flarum-suspend.forum.suspend_user.reason')}
-              <textarea className="FormControl" bidi={this.reason} placeholder="optional" rows="2"/>
-            </label>
-          </div>
-
-          <div className="Form-group">
-            <label>
-              {app.translator.trans('flarum-suspend.forum.suspend_user.display_message')}
-              <textarea className="FormControl" bidi={this.message} placeholder="optional" rows="2"/>
-            </label>
-          </div>
-
           <div className="Form-group">
             <Button className="Button Button--primary" loading={this.loading} type="submit">
               {app.translator.trans('flarum-suspend.forum.suspend_user.submit_button')}
@@ -95,6 +53,72 @@ export default class SuspendUserModal extends Modal {
         </div>
       </div>
     );
+  }
+
+  formItems() {
+    const items = new ItemList();
+
+    items.add(
+      'not-suspended',
+      <label className="checkbox">
+        <input type="radio" name="status" checked={!this.status()} value="" onclick={withAttr('value', this.status)} />
+        {app.translator.trans('flarum-suspend.forum.suspend_user.not_suspended_label')}
+      </label>,
+      100
+    );
+
+    items.add(
+      'indefinitely',
+      <label className="checkbox">
+        <input type="radio" name="status" checked={this.status() === 'indefinitely'} value='indefinitely' onclick={withAttr('value', this.status)} />
+        {app.translator.trans('flarum-suspend.forum.suspend_user.indefinitely_label')}
+      </label>,
+      90
+    );
+
+    items.add(
+      'time-suspension',
+      <label className="checkbox SuspendUserModal-days">
+        <input type="radio" name="status" checked={this.status() === 'limited'} value='limited' onclick={e => {
+          this.status(e.target.value);
+          m.redraw.sync();
+          this.$('.SuspendUserModal-days-input input').select();
+          e.redraw = false;
+        }} />
+        {app.translator.trans('flarum-suspend.forum.suspend_user.limited_time_label')}
+        {this.status() === 'limited' ? (
+          <div className="SuspendUserModal-days-input">
+            <input type="number"
+              min="0"
+              value={this.daysRemaining()}
+              oninput={withAttr('value', this.daysRemaining)}
+              className="FormControl" />
+            {app.translator.trans('flarum-suspend.forum.suspend_user.limited_time_days_text')}
+          </div>
+        ) : ''}
+      </label>,
+      80
+    );
+
+    items.add(
+      'reason',
+      <label>
+        {app.translator.trans('flarum-suspend.forum.suspend_user.reason')}
+        <textarea className="FormControl" bidi={this.reason} placeholder="optional" rows="2" />
+      </label>,
+      70
+    );
+
+    items.add(
+      'message',
+      <label>
+        {app.translator.trans('flarum-suspend.forum.suspend_user.display_message')}
+        <textarea className="FormControl" bidi={this.message} placeholder="optional" rows="2" />
+      </label>,
+      60
+    );
+
+    return items;
   }
 
   onsubmit(e) {
@@ -114,10 +138,10 @@ export default class SuspendUserModal extends Modal {
         break;
 
       default:
-        // no default
+      // no default
     }
 
-    this.attrs.user.save({suspendedUntil: suspendedUntil, suspendReason: this.reason(), suspendMessage: this.message()}).then(
+    this.attrs.user.save({ suspendedUntil: suspendedUntil, suspendReason: this.reason(), suspendMessage: this.message() }).then(
       () => this.hide(),
       this.loaded.bind(this)
     );
